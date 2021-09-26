4009e6959bf1b85b05d621298ae5f0e780f2ba16
test3207 <test3207@outlook.com>
2021-09-26 15:37:16 +0800

    update for lint and avoid mem leak

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
-const EventEmitter = require('events')
+const EventEmitter = require('events');
 module.exports = class extends EventEmitter {
-  constructor(processor) {
-    super()
-    if (typeof processor !== 'function') throw new Error('Wrong Processor Type!')
-    this.processor = processor
+    constructor(processor) {
+        super();
+        if (typeof processor !== 'function') throw new Error('Wrong Processor Type!');
+        this.processor = processor;
 
-    this.queue = []
-    this.status = 0 // 0为执行完毕，1为执行中，用以避免并发add时多次调用
-    this.on('ready', async () => {
-      if (this.status !== 1) {
-        this.status = 1
-        const job = this.queue.shift()
-        if (job) {
-          await this.processor(job)
-        }
-        this.status = 0
-        !this._close && this.queue.length && this.emit('ready')
-      }
-    })
-  }
+        this.queue = [];
+        this.pending = false;
+        this.on('ready', async () => {
+            if (!this.pending) {
+                this.pending = true;
+                const job = this.queue.shift();
+                if (job) {
+                    await this.processor(job);
+                }
+                this.pending = false;
+                this.queue.length && this.emit('ready');
+            }
+        });
+    }
 
-  add (job) {
-    this.queue.push(job)
-    this.emit('ready')
-  }
+    add(job) {
+        this.queue.push(job);
+        this.emit('ready');
+    }
 
-  close() {
-    this._close = true
-  }
+    close() {
+        this.removeAllListeners();
+    }
 }
